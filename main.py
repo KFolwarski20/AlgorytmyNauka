3693e511e18f291fd143aa5a363bf3f18763277a
KFolwarski20 <konfolw@wp.pl>
2026-01-29 22:40:27 +0100

    Aktualizacja plików po zakończeniu rozdziału drugiego

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from introductionToAlgorithms import binary_search
+from sortingBySelection import selection_sort
 
 if __name__ == '__main__':
     my_list = [1, 3, 5, 7, 9]
@@ -7,3 +8,4 @@ if __name__ == '__main__':
     print(f"Item: {binary_search(my_list, 3)}|\t{time.time() - start_time_1:.8f} seconds")
     start_time_2 = time.time()
     print(f"Item: {binary_search(my_list, -1)}|\t{time.time() - start_time_2:.8f} seconds")
+    print(f"Selection sort: {selection_sort([5, 3, 6, 2, 10])}")
--- sortingBySelection.py
+++ sortingBySelection.py
@@ -64,3 +64,21 @@ czy ta nowa struktura będzie szybsza, czy wolniejsza.
 # Wstawianie elementów będzie szybsze niż w tablicach, tak samo szybkie jak w listach powiązanych. Utworzona struktura
 # jest wolniejsza od tablic, jeśli chodzi o wyszukiwanie, ale szybsza od nich i tak samo szybka jak listy powiązane we
 # wszystkich operacjach. Tablice i listy powiązane są cegiełkami, z którym tworzy się bardziej złożone struktury danych.
+
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
