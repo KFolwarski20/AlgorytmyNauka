9a44eea40d437f732b202dbc555a74d381ee100e
KFolwarski20 <konfolw@wp.pl>
2026-02-02 22:33:48 +0100

    Przeniesienie zadań z pliku quicksort.py do dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+"""
+Które z poniższych funkcji są konsekwentne w tej kwestii?
+5.1. f(x) = 1 <- zwraca 1 dla wszystkich danych wejściowych.
+5.2. f(x) = rand() <- za każdym razem zwraca losową liczbę.
+5.3. f(x) = next_empty_slot() <- zwraca indeks następnej pustej komórki w tablicy skrótów.
+5.4. f(x) = len(x) <- jako indeksu używa długości łańcucha.
+"""
+
+# 5.1. Odpowiedź: konsekwentna
+# 5.2. Odpowiedź: niekonsekwentna
+# 5.3. Odpowiedź: niekonsekwentna
+# 5.4. Odpowiedź: konsekwentna
